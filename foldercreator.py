a869cdd8f9e64158c46dc1ded972f904858f3648
Ertuğrul Akın <43358488+akinertu@users.noreply.github.com>
2023-01-30 22:50:25 +0300

    Update foldercreator.py

--- foldercreator.py
+++ foldercreator.py
@@ -3,6 +3,6 @@ import os
 
 print(Path.home()/Path('Desktop/2301 Şub'))
 
-os.makedirs(Path.home()/Path('Desktop/2301 Şub'))
+#os.makedirs(Path.home()/Path('Desktop/2301 Şub'))
 for i in range (1,32):
     os.makedirs(Path.home()/Path('Desktop/2302 Şub/'+str(i)))
