e69992d5b2b26c8ecc80582c982e5749754d64bb
Ertuğrul Akın <43358488+akinertu@users.noreply.github.com>
2023-01-30 22:46:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sehirdecaz
+ 
---
+++ foldercreator.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+import os
+
+print(Path.home()/Path('Desktop/2301 Şub'))
+
+os.makedirs(Path.home()/Path('Desktop/2301 Şub'))
+for i in range (1,32):
+    os.makedirs(Path.home()/Path('Desktop/2302 Şub/'+str(i)))
