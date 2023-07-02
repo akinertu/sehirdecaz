69ac113a1e91d2d1f8a77156ea3f6329a746e692
Ertuğrul Akın <43358488+akinertu@users.noreply.github.com>
2023-07-02 12:11:58 +0300

    Selenium
    Selenium used to parse html and calendar data retrived.

--- nardis.py
+++ nardis.py
@@ -1,19 +1,48 @@
-#! python3
-# searchpypi.py  - Opens several search results.
+import json
+import pandas as pd
+import time
 
-import requests, webbrowser, bs4
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+driver.get("https://nardisjazz.com/")
+driver.implicitly_wait(30)
+time.sleep(5)
 
-res = requests.get('https://nardisjazz.com/')
-res.raise_for_status()
+evcal_list = driver.find_element(by=By.ID, value="evcal_list")
+children = evcal_list.get_property("children")
 
-soup = bs4.BeautifulSoup(res.text, 'html.parser')
-linkElems = soup.select('#evcal_list')
-a = linkElems[0].get('class')
+def replace_text(string):
+    rep = ["\n\t\t\t\t\t", "<strong>","\xa0", "&quot;"]
+    for e in rep:
+        string = string.replace(e,"")
+    string = string.replace("- scroll down for English   ","\",\"lineup\":\"")
+    string = string.split("</strong>")[0] + "\"}"
+    return string
 
-"""
-numOpen = min(5, len(linkElems))
-for i in range(numOpen):
-    urlToOpen = linkElems[i].get('href')
-    print('Opening', urlToOpen)
-    webbrowser.open(urlToOpen)
-    """
+dict_list = []
+for e in children:
+    string = e.get_property("textContent")
+    string2 = replace_text(string)
+    try:
+        child_dict = json.loads(string2)
+        dict_list.append(child_dict)
+    except:
+        print(dict_list[-1]["name"])
+        print(string2)
+
+event_list = []
+for e in dict_list:
+    event_dict = {}
+    event_dict["date"] = e["startDate"].split("T")[0]
+    event_dict["time"] = e["startDate"].split("T")[1].split("+")[0]
+    event_dict["name"] = e["name"].title()
+    event_dict["lineup"] = e["lineup"]
+    event_dict["url"] = e["url"]
+    event_list.append(event_dict)
+
+event_df = pd.DataFrame()
+for e in event_list:
+    event_df = pd.concat([event_df,pd.DataFrame([e])], ignore_index=True)
+event_df["date"] = pd.to_datetime(event_df["date"])
+event_df.to_excel('nardis.xlsx')
